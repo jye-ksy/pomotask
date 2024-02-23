7fa6d52f8cd011488cea65b3b48b33fb58413456
ksy415 <sykevin415@gmail.com>
2024-02-22 21:30:54 -0800

    add navbar with clerk sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,29 @@
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
+import { SignInButton, SignOutButton, auth } from "@clerk/nextjs";
 
-import { CreatePost } from "~/app/_components/create-post";
-import { api } from "~/trpc/server";
-
-export default async function Home() {
-  noStore();
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+async function Navbar() {
+  const { userId } = auth();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <nav className="flex h-16 items-center gap-4 border-b px-4 lg:gap-8">
+      <Link className="text-lg font-medium" href="#">
+        Pomofocus
+      </Link>
+      <div className="ml-auto flex items-center gap-4">
+        <div className="text-sm font-medium underline transition-colors hover:text-gray-900">
+          {userId ? <SignInButton /> : <SignOutButton />}
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
-
-        <CrudShowcase />
       </div>
-    </main>
+    </nav>
   );
 }
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
+export default async function Home() {
+  noStore();
 
   return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
+    <main className="min-h-screen ">
+      <Navbar />
+    </main>
   );
 }
