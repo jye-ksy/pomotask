7200705fb32cafb4269926a2d04e17b0e8e4459c
James Ye <james101994@yahoo.com>
2024-02-27 14:19:41 -0800

    Update styling for project cards

--- src/app/_components/NewProjectCard.tsx
+++ src/app/_components/NewProjectCard.tsx
@@ -5,7 +5,7 @@ export default function NewProjectCard() {
 
     return <Link  
             href='/create/project' 
-            className="flex justify-center items-center border-solid border-[1px] border-black rounded-md p-4 min-w-[350px] max-w-[350px]">
+            className="flex justify-center items-center bg-gray-50 drop-shadow-md border-solid border-[1px] border-grey-200 rounded-md p-4 min-w-[350px] max-w-[350px]">
             Add New Project
         </Link>
 }
--- src/app/_components/ProjectCard.tsx
+++ src/app/_components/ProjectCard.tsx
@@ -1,6 +1,6 @@
 export default function ProjectCard() {
 
-    return <div className="border-solid border-[1px] border-black rounded-md p-4 min-w-[350px] max-w-[350px]">
+    return <div className="bg-gray-50 drop-shadow-md border-solid border-[1px] border-grey-200 rounded-md p-4 min-w-[350px] max-w-[350px]">
         <h1 className="font-bold text-md">Project Title</h1>
         <div>
             <p>A project </p>
