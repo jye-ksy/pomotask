854cb8a8c5dc820a4783c028bfdd3ebbf4842d88
ksy415 <sykevin415@gmail.com>
2024-03-15 15:15:21 -0700

    Fix task creation not working when project is not assigned

--- src/server/api/routers/task.ts
+++ src/server/api/routers/task.ts
@@ -1,58 +1,83 @@
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const taskRouter = createTRPCRouter({
   create: protectedProcedure
     .input(
-        z.object({
-            name: z.string().min(2).max(250, {
-              message: "Project needs a name",
-            }),
-            status: z.enum(['NOT_STARTED', 'IN_PROGRESS', 'COMPLETE']),
-            due: z.date(), 
-            priority: z.enum(['LOW', 'MEDIUM', 'HIGH']),
-            notes: z.union([z.string().length(0), z.string().min(4)])
-            .optional()
-            .transform(e => e === "" ? undefined : e),
-            projectId: z.string()
-          })
+      z.object({
+        name: z.string().min(2).max(250, {
+          message: "Project needs a name",
+        }),
+        status: z.enum(["NOT_STARTED", "IN_PROGRESS", "COMPLETE"]),
+        due: z.date().optional(),
+        priority: z.enum(["LOW", "MEDIUM", "HIGH"]),
+        notes: z
+          .union([z.string().length(0), z.string().min(4)])
+          .optional()
+          .transform((e) => (e === "" ? undefined : e)),
+        projectId: z.string(),
+      }),
     )
-    .mutation(async ({ ctx, input: {name, due, status, priority, notes, projectId } }) => {
-
-      return ctx.db.task.create({
-        data: {
+    .mutation(
+      async ({
+        ctx,
+        input: { name, due, status, priority, notes, projectId },
+      }) => {
+        let task = await ctx.db.task.create({
+          data: {
             status,
             name,
-            due,
             priority,
             notes,
-            project: {
-              connect: {
-                id: projectId
-              }
-            },
             user: {
               connect: {
-                id: ctx.userId
-              }
-            }
-        },
-      });
-    }),
+                id: ctx.userId,
+              },
+            },
+          },
+        });
 
-
-    getAllUserTasks: protectedProcedure
-    .query(async ({ctx}) => {
-      return ctx.db.task.findMany({
-        where: {
-          userId: ctx.userId
+        // Link to project if it exists
+        if (projectId) {
+          task = await ctx.db.task.update({
+            where: {
+              id: task.id,
+            },
+            data: {
+              project: {
+                connect: {
+                  id: projectId,
+                },
+              },
+            },
+            include: {
+              project: true,
+            },
+          });
         }
-      })
-    }),
 
+        // Add due date if it exists
+        if (due) {
+          task = await ctx.db.task.update({
+            where: {
+              id: task.id,
+            },
+            data: {
+              due,
+            },
+          });
+        }
+
+        return task;
+      },
+    ),
+
+  getAllUserTasks: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.task.findMany({
+      where: {
+        userId: ctx.userId,
+      },
+    });
+  }),
 });
