6e527f7b9794e49ee6a4a0775b399262be453388
ksy415 <sykevin415@gmail.com>
2024-03-25 10:29:55 -0700

    fix weird ui bug on component mount

--- src/components/ui/card-textarea.tsx
+++ src/components/ui/card-textarea.tsx
@@ -20,10 +20,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
           ref.style.height = "auto";
           // Set the default height to 1 line
           ref.style.height = "40px";
-
-          if (ref?.scrollHeight > 40) {
-            ref.style.height = ref?.scrollHeight + "px";
-          }
+          ref.style.height = ref?.scrollHeight + "px";
         }
       };
 
@@ -35,7 +32,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
+          "flex h-[40px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
           className,
         )}
         ref={mergeRefs([textAreaRef, ref])}
