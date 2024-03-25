ed02e559d82fa8f099beb9b4779b1e9a17b98c96
ksy415 <sykevin415@gmail.com>
2024-03-25 12:29:30 -0700

    make navbar narrower

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -2,31 +2,34 @@ import Link from "next/link";
 import { SignInButton, SignOutButton, UserButton, auth } from "@clerk/nextjs";
 
 export async function Navbar() {
-    const { userId } : { userId: string | null } = auth();
-  
-    return (
-      <nav className="flex h-16 items-center gap-4 border-b px-8 lg:gap-8 z-2">
+  const { userId }: { userId: string | null } = auth();
+
+  return (
+    <nav className="z-2 flex h-16 items-center justify-center gap-4  px-8 lg:gap-8">
+      <div className="mt-1 flex w-full max-w-7xl items-center border-b px-1 py-4">
         <Link className="text-lg font-medium" href="">
           Pomotask
         </Link>
         <div className="ml-auto flex items-center gap-4">
           <div className="text-sm font-medium transition-colors ">
-            {userId ? 
-            <div className="flex items-center gap-4">
-              <Link className="hover:text-gray-400" href="/dashboard">
-                Dashboard
-              </Link>
-              <Link className="hover:text-gray-400" href="/create/project">
-                New Project
-              </Link>
-              <div className="ml-auto"><UserButton afterSignOutUrl="/"/></div>
-              {/* <SignOutButton/>  */}
+            {userId ? (
+              <div className="flex items-center gap-4">
+                <Link className="hover:text-gray-400" href="/dashboard">
+                  Dashboard
+                </Link>
+                <div className="ml-auto">
+                  <UserButton afterSignOutUrl="/" />
+                </div>
+                {/* <SignOutButton/>  */}
               </div>
-            :<Link className="hover:text-gray-400" href="sign-in">
-            Sign In
-          </Link>}
+            ) : (
+              <Link className="hover:text-gray-400" href="sign-in">
+                Sign In
+              </Link>
+            )}
           </div>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
+}
